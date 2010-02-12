33d225cb1267b1d38449f601b19c12f1ade3286a
Bjrn Nordlund <bjornno@gmail.com>
2010-02-12 21:48:56 +0100

    jup

--- src/main/resources/archetype-resources/src/main/java/service/Process.java
+++ src/main/resources/archetype-resources/src/main/java/service/Process.java
@@ -3,8 +3,9 @@ package ${groupId}.service;
 import bjornno.asynchwork.Message;
 import bjornno.asynchwork.ReceivingService;
 
-public class Process {
+public class Process implements ReceivingService {
     public void receive(Message message) {
+        System.out.println("Recieving message: ");
         System.out.println(message.getPayload());
     }
     public String getReceiverName() {
